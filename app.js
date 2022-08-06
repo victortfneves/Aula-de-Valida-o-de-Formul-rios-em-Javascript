e0f5723364e7a184bade5fb0bf116db7c11b0380
Victor Teixeira Ferreira Neves <victortfneves@gmail.com>
2022-08-06 15:00:16 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import{valida} './validacao.js'
+const inputs = documento.querySelectorAll('input');
+inputs.forEach(input => {
+    if(input.dataset.tipo === 'preco'){
+        SimpleMaskMoney.setMask(input,{
+                    
+            prefix: 'R$ ',
+            fixed: true,
+            fractionDigits: 2,
+            decimalSeparator: ',',
+            thousandsSeparator: '.',
+            cursor: 'end'
+        })
+    }
+    input.addEventListener('blur',(evento) => {
+        valida(evento.target)
+    })
+})
